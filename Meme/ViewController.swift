3c3210f66bb1f3a410b8b830871ebe7b87452877
avo1 <voanhdung@gmail.com>
2015-12-01 02:01:23 +0700

    Udacity Meme

--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  @IBOutlet weak var imagePickerView: UIImageView!
+  @IBOutlet weak var cameraButton: UIBarButtonItem!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    // User selected an image
+    if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+      self.imagePickerView.image = image
+    }
+    self.dismissViewControllerAnimated(true, completion: nil)
   }
-
-
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    // User cancel the image picker
+    self.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+    self.presentViewController(imagePicker, animated: true, completion: nil)
+  }
+  
+  @IBAction func takePicFromCamera(sender: AnyObject) {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+    self.presentViewController(imagePicker, animated: true, completion: nil)
+  }
+  
 }
-
